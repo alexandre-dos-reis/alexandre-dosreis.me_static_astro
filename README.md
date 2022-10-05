0cada882c830e382e9aa5199af6750b9b7f7ce1c
Alex <ajm.dosreis.daponte@gmail.com>
2022-10-06 00:05:37 +0200

    Ok

---
+++ README.md
@@ -0,0 +1,7 @@
+# alexandre-dosreis.me
+
+## Links
+
+[https://tailwindcomponents.com/cheatsheet/](Tailwind Cheat Sheet)
+[https://emojiterra.com/laptop-computer/](Emoji)
+[https://www.copyandpastesymbols.net/](Emoji)
---
+++ src/utils/tech-stack.ts
@@ -0,0 +1,18 @@
+// export interface TechStack {
+//   color: string;
+//   name: string;
+// }
+
+// export const techStacks: TechStack[] = [
+//   {
+//     color: "#97890C",
+//     name: "NextJS",
+//   },
+//   {
+//     color: "#68B0F4",
+//     name: "Symfony"
+//   },
+//   {
+//     color: ""
+//   }
+// ];
