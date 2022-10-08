09aa7546e7f8bafcda4b2f696afb4a7ad5db6a04
Alex <ajm.dosreis.daponte@gmail.com>
2022-10-08 23:24:37 +0200

    Remove mdx compo to transform a tag to Link comp, behavior is now done by rehype plugin

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,8 @@ import tailwind from "@astrojs/tailwind";
 import mdx from "@astrojs/mdx";
 import remarkMdxImages from 'remark-mdx-images';
 import remarkGfm from 'remark-gfm';
+import wrap from 'rehype-wrap'
+import urls from 'rehype-urls'
 
 // https://astro.build/config
 export default defineConfig({
@@ -11,7 +13,13 @@ export default defineConfig({
       // https://github.com/shikijs/shiki/blob/main/docs/themes.md#all-themes
       theme: 'material-darker'
     },
-    remarkPlugins: [remarkMdxImages, remarkGfm]
+    remarkPlugins: [remarkMdxImages, remarkGfm],
+    rehypePlugins: [[wrap, {selector: 'table', wrapper: 'div.overflow-x-auto'}], [urls, (url, node) => {
+      if (url.href.startsWith('http')) {
+        node.properties.target = '_blank'
+        node.properties.rel = 'noreferrer noopener'
+      }
+    }]]
   },
   integrations: [tailwind(), mdx()]
 });
