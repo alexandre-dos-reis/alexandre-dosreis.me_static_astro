b8609b9ac4a4282b255b9dc933ae637f5895094d
Alex <ajm.dosreis.daponte@gmail.com>
2022-10-08 19:00:16 +0200

    Headings in blog article atr now displayed properly

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,7 @@
 import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
 import mdx from "@astrojs/mdx";
+import remarkMdxImages from 'remark-mdx-images';
 
 // https://astro.build/config
 export default defineConfig({
@@ -8,7 +9,8 @@ export default defineConfig({
     shikiConfig: {
       // https://github.com/shikijs/shiki/blob/main/docs/themes.md#all-themes
       theme: 'material-darker'
-    }
+    },
+    remarkPlugins: [remarkMdxImages]
   },
   integrations: [tailwind(), mdx()]
 });
