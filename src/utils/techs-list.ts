9bb9b4d034fcf220a3ab25414a80451ee405ab75
Alex <ajm.dosreis.daponte@gmail.com>
2022-10-21 16:34:14 +0200

    New TechStack component

--- src/utils/techs-list.ts
+++ src/utils/techs-list.ts
@@ -1,36 +1,30 @@
-export const techsList: { tech: string; color: string }[] = [
-  {
-    tech: "Symfony",
-    color: "bg-[#1d4fd8]",
-  },
-  {
-    tech: "MariaDB",
-    color: "bg-[#E35169]",
-  },
-  {
-    tech: "NextJS",
-    color: "bg-[#97890C]",
-  },
-  {
-    tech: "Preact",
-    color: "bg-[#9E8DE4]",
-  },
-  {
-    tech: "Web Components",
-    color: "bg-[#F7971C]",
-  },
-  {
-    tech: "React",
-    color: "bg-[#00C897]",
-  },
-  {
-    tech: "Directus 9",
-    color: "bg-[#607D8B]",
-  },
-  {
-    tech: "Astro",
+export type TechType =
+  | "nextjs"
+  | "symfony"
+  | "mariadb"
+  | "preact"
+  | "webcomponent"
+  | "react"
+  | "directus9"
+  | "astro"
+  | "typescript"
+  | "nodejs"
+  | (string & {});
+
+export type Tech = { label: string; color: string };
+
+export const techList: Record<TechType, Tech> = {
+  astro: {
+    label: "Astro",
     color: "bg-[#65219e]",
   },
-  { tech: "Typescript", color: "bg-[#3379c6]" },
-  { tech: "NodeJS", color: "bg-[#3d863d]" },
-];
+  directus9: { label: "Directus 9", color: "bg-[#607D8B]" },
+  nodejs: { label: "NodeJS", color: "bg-[#3d863d]" },
+  mariadb: { label: "MariaDB", color: "bg-[#E35169]" },
+  nextjs: { label: "NextJS", color: "bg-[#97890C]" },
+  preact: { label: "Preact", color: "bg-[#9E8DE4]" },
+  react: { label: "React", color: "bg-[#00C897]" },
+  symfony: { label: "Symfony", color: "bg-[#1d4fd8]" },
+  typescript: { label: "Typescript", color: "bg-[#3379c6]" },
+  webcomponent: { label: "Web Components", color: "bg-[#F7971C]" },
+};
